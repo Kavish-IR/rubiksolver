eed32e6af2240bee01de107491121d39e7b4385a
Paul Kepley <pakepley@gmail.com>
2021-10-09 16:51:45 -0500

    Reverting to fix bug introduced in commit 774e042.

--- src/rubiksolver/__init__.py
+++ src/rubiksolver/__init__.py
@@ -1,12 +1,13 @@
-from .end_to_end import main
-from .ManualSquareExtractor import ManualSquareExtractor
-from .SolutionGallery import SolutionGallery
-from .Solver import Solver
-from .Cube import Cube
-from .Cube import plt
 import os
 import sys
 
 # Read the install dir to know where training data is
 from .config import install_dir
 sys.path.append(install_dir)
+
+from .Cube import plt
+from .Cube import Cube
+from .Solver import Solver
+from .SolutionGallery import SolutionGallery
+from .ManualSquareExtractor import ManualSquareExtractor
+from .end_to_end import main
